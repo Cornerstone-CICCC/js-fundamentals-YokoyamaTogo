b38db287a1df3c288225c2ecbcc5c51142bd5ea8
kirakira <15catinthebox@gmail.com>
2026-01-21 12:30:14 -0800

    ex 04

--- exercises/ex04.js
+++ exercises/ex04.js
@@ -9,5 +9,17 @@ If the above conditions are true, then print out:
 I'd like two scoops of __________ ice cream in a __________ with __________.
 Fill in the blanks with the flavor of the ice cream, vessel, and toppings. For example,
 
-I'd like two scoops of vanilla ice cream in a cone with peanuts. 
+I'd like two scoops of vanilla ice cream in a cone with peanuts.
 */
+
+const flavor = "vanilla"
+const vessel = "cone"
+const toppings = "sprinkles"
+
+isCorrectFlavor = flavor === "vanilla" || flavor === "chocolate"
+isCorrectVessel = vessel === "cone" || vessel === "bowl"
+isCorrectToppings = toppings === "sprinkles" || toppings === "peanuts"
+
+if (isCorrectFlavor && isCorrectVessel && isCorrectToppings) {
+  console.log(`I'd like two scoops of ${flavor} ice cream in a ${vessel} with ${toppings}.`)
+}
