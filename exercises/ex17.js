336a1f215280ca5fb7414171619ec0e316ccdb37
kirakira <15catinthebox@gmail.com>
2026-01-22 11:35:56 -0800

    ex 17

--- exercises/ex17.js
+++ exercises/ex17.js
@@ -4,3 +4,17 @@ Write an anonymous function expression that stores a function in a variable call
 laugh(3);
 Returns: hahaha!
 */
+
+const laugh = function (num) {
+  let str = ''
+
+  let i = 1
+  while (i <= num) {
+    str += 'ha'
+    i++
+  }
+
+  console.log(`${str}!`)
+}
+
+laugh(4)
