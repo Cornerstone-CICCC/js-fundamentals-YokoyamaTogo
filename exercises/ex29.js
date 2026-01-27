031ce6468f7262fb350e4bbe90bb8b17ce6f172c
kirakira <15catinthebox@gmail.com>
2026-01-26 16:56:44 -0800

    ex 29

--- exercises/ex29.js
+++ exercises/ex29.js
@@ -15,3 +15,15 @@ Chocolate donuts cost $2.45 each
 Cider donuts cost $1.59 each
 Boston Cream donuts cost $5.99 each
 */
+
+var donuts = [
+  { type: "Jelly", cost: 1.22 },
+  { type: "Chocolate", cost: 2.45 },
+  { type: "Cider", cost: 1.59 },
+  { type: "Boston Cream", cost: 5.99 }
+]
+
+for (const index in donuts) {
+  const donut = donuts[index]
+  console.log(`${donut.type} donuts cost $${donut.cost} each`)
+}
