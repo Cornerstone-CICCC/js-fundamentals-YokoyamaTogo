77da9606b8988c1d72a8e89a64e5d25c791bc847
kirakira <15catinthebox@gmail.com>
2026-01-26 16:17:47 -0800

    ex 21

--- exercises/ex21.js
+++ exercises/ex21.js
@@ -6,3 +6,10 @@ TIP: The 1st element of any array has an index of 0.
 
 Afterwards, print out the prices array to the console.
 */
+
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00]
+prices[0] = 0
+prices[2] = 0
+prices[6] = 0
+
+console.log(prices)
