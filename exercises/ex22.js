e18778b8901df8399772b287b1ddd52f78728065
kirakira <15catinthebox@gmail.com>
2026-01-26 16:17:58 -0800

    ex 22

--- exercises/ex22.js
+++ exercises/ex22.js
@@ -1,5 +1,6 @@
 /*
-James was creating an array with the colors of the rainbow, and he forgot some colors. The standard rainbow colors are usually listed in this order:
+James was creating an array with the colors of the rainbow, and he forgot some colors.
+The standard rainbow colors are usually listed in this order:
 
 var rainbow = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple"];
 but James had this:
@@ -11,3 +12,11 @@ Remove "Blackberry"
 Add "Yellow" and "Green"
 Add "Purple"
 */
+
+var rainbow = ["Red", "Orange", "Blackberry", "Blue"]
+
+rainbow.splice(2, 1)
+rainbow.splice(2, 0, "Yellow", "Green")
+rainbow.splice(5, 0, "Purple")
+
+console.log(rainbow)
